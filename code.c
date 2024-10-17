77a63deab5785118b6e5833278555552d00e893a
sivapriyasanda <sivapriyasanda123@gmail.com>
2024-10-17 12:43:08 +0530

    some changes

--- code.c
+++ code.c
@@ -2,7 +2,7 @@
 
 int main() {
     char operator;
-    double num1, num2, result;
+    double num1, num2, results;
 
     // Input operator from the user
     printf("Enter an operator (+, -, *, /): ");
@@ -15,17 +15,17 @@ int main() {
     // Perform calculation based on the operator
     switch (operator) {
         case '+':
-            result = num1 + num2;
+            results = num1 + num2;
             break;
         case '-':
-            result = num1 - num2;
+            results = num1 - num2;
             break;
         case '*':
-            result = num1 * num2;
+            results = num1 * num2;
             break;
         case '/':
             if (num2 != 0)
-                result = num1 / num2;
+                results = num1 / num2;
             else {
                 printf("Error! Division by zero is not allowed.\n");
                 return -1;
@@ -37,7 +37,7 @@ int main() {
     }
 
     // Output the result
-    printf("Result: %.2lf\n", result);
+    printf("Result: %.2lf\n", results);
 
     return 0;
 }
